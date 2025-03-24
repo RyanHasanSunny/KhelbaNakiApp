9063520285cf603a1bd3c72df2aa26d6a6c3fa2d
Ryan Hasan Sunny <ryanhasansunny31@gmail.com>
2025-03-25 05:29:39 +0600

    setup project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LoginPage from "./components/front-end/login-page/LoginPage";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App flex justify-center items-center h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          {/* Add more routes here if needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
